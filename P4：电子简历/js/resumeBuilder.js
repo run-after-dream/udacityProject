a05dacb899e1696590a61786ca8b9388f4fa01a5
zili chen <zili chen>
2018-02-12 15:07:03 +0800

    secong commit

--- P4：电子简历/js/resumeBuilder.js
+++ P4：电子简历/js/resumeBuilder.js
@@ -13,7 +13,7 @@ var bio = {
 	"welcomeMessage":"Come On!",
 	"skills":skills,
 	"biopic":"images/me.jpg",
-	"display":function displaayBio()
+	"display":function()
 	{
 		var formattedName = HTMLheaderName.replace("%data%","James Chen");
 		var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
@@ -21,15 +21,15 @@ var bio = {
 		$("#header").prepend(formattedName);
 
 		var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
-		$("#topContacts").append(formattedMobile);
+		$("#topContacts,#footerContacts").append(formattedMobile);
 		var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
-		$("#topContacts").append(formattedEmail);
+		$("#topContacts,#footerContacts").append(formattedEmail);
 		var formattedGithub = HTMLgithub.replace("%data%",bio.contacts.github);
-		$("#topContacts").append(formattedGithub);
+		$("#topContacts,#footerContacts").append(formattedGithub);
 		var formattedTwitter = HTMLtwitter.replace("%data%",bio.contacts.twitter);
-		$("#topContacts").append(formattedTwitter);
+		$("#topContacts,#footerContacts").append(formattedTwitter);
 		var formattedLocation = HTMLlocation.replace("%data%",bio.contacts.location);
-		$("#topContacts").append(formattedLocation);
+		$("#topContacts,#footerContacts").append(formattedLocation);
 		var formattedBioPic = HTMLbioPic.replace("%data%",bio.biopic);
 		$("#header").append(formattedBioPic);
 		var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
@@ -38,9 +38,8 @@ var bio = {
 		if(bio.skills.length > 0)
 		{
 			$("#header").append(HTMLskillsStart);
-			for (const i in bio.skills) 
-			{
-				$("#header").append(HTMLskills.replace("%data%",bio.skills[i]));
+			for (var i = 0; i <= bio.skills.length - 1; i++) {
+				$("#skills").append(HTMLskills.replace("%data%",bio.skills[i]));
 			}
 		}
 	}	
@@ -51,7 +50,7 @@ var education = {
 		"name":"A",
 		"location":"南京市",
 		"degree":"master",
-		"majors":"Electronics and communications engineering",
+		"majors":["Electronics and communications engineering"],
 		"dates":"2015.9-2018.6",
 		"url":"www.google.com"
 	},
@@ -59,7 +58,7 @@ var education = {
 		"name":"B",
 		"location":"常州市",
 		"degree":"bechalor",
-		"majors":"Electronics and communications engineering",
+		"majors":["Electronics and communications engineering"],
 		"dates":"2011.9-2015.6",
 		"url":"www.google.com"
 	}],
@@ -69,10 +68,10 @@ var education = {
 		"dates":"2017.8-2018.2",
 		"url":"http://cn.udacity.com"
 	}],
-	"display":function displaayEducation()
+	"display":function()
 	{
 		$("#education").append(HTMLschoolStart);
-		for (const i in education.schools) 
+		for (var i = 0; i <= education.schools.length - 1; i++)
 		{
 			var formattedName = HTMLschoolName.replace("%data%",education.schools[i].name);
 			$(".education-entry:last").append(formattedName);
@@ -82,11 +81,15 @@ var education = {
 			$(".education-entry:last").append(formattedDates);
 			var formattedLocation = HTMLschoolLocation.replace("%data%",education.schools[i].location);
 			$(".education-entry:last").append(formattedLocation);
-			var formattedMajor = HTMLschoolMajor.replace("%data%",education.schools[i].majors);
-			$(".education-entry:last").append(formattedMajor);
+			for (var j = 0; j <= education.schools[i].majors.length - 1; j++)
+			{
+				var formattedMajor = HTMLschoolMajor.replace("%data%",education.schools[i].majors[j]);
+				$(".education-entry:last").append(formattedMajor);
+			}
 		}
+		
 		$(".education-entry:last").append(HTMLonlineClasses);
-		for (const i in education.onlineCourses) 
+		for (var i = 0; i <= education.onlineCourses.length - 1; i++)
 		{
 			var formattedOnlineTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses[i].title);
 			$(".education-entry:last").append(formattedOnlineTitle);
@@ -109,11 +112,10 @@ var work = {
 		"dates":"in progress",
 		"description":" I began work for a public company as a software engineer in NanJing.Because Im capable of more responsibilities, so I decided to change my job. And in August 2018,I left NanJing to ShangHai and worked for a foreign enterprise as a software engineer."
 	}],
-	"display":function displayWork()
+	"display":function()
 	{
 		$("#workExperience").append(HTMLworkStart);
-		for (const i in work.jobs) 
-		{
+		for (var i = 0; i <= work.jobs.length - 1; i++) {
 			var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[i].employer);
 			var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[i].title);
 			var formattedEmployerTitle = formattedEmployer + formattedTitle;
@@ -140,11 +142,10 @@ var project = {
 		"description":"This project use HTML,CSS and JS to create a web page which title is journey.",
 		"images":["images/proj21.JPG","images/proj22.JPG"]
 	}],
-	"display":function displayProject()
+	"display":function()
 	{
 		$("#projects").append(HTMLprojectStart);
-		for (const i in project.projects) 
-		{
+		for (var i = 0; i <= project.projects.length - 1; i++){
 			var formattedTitle = HTMLprojectTitle.replace("%data%",project.projects[i].title);
 			$(".project-entry:last").append(formattedTitle);
 			var formattedDates = HTMLprojectDates.replace("%data%",project.projects[i].dates);
@@ -154,13 +155,13 @@ var project = {
 
 			if(project.projects[i].images.length > 0)
 			{
-				for (const j in project.projects[i].images) 
-				{
+				for (var j = 0; j <= project.projects[i].images.length - 1; j++){
 					var formattedImage = HTMLprojectImage.replace("%data%",project.projects[i].images[j]);
 					$(".project-entry:last").append(formattedImage);
 				}
 			}
 		}
+		
 	}
 };
 
